f9f902371dd5a3e1d6cbb7e3e134f3bc7d805eb9
Ashutosh kumar gautam <65886012+Ashutoshkumargautam@users.noreply.github.com>
2020-10-09 17:06:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,11 @@
-#===================================[program logic]============================================#
-""" When new email comes then bot will capture that email and doing work it own way
-    if any email is not coming so that time bot will waitting for intire time."""
-#================================================================================================#
 from time import sleep
-import imaplib, email
+import imaplib
 import time
 import sys
-#===============================================================================>>
-#=============[Reading email here..]===============================>>
-import imaplib
 import email
 
-email_address = 'xyzxxxaxbxzx123@gmail.com' # Placeholder
-email_pass = 'xxx@123456' # Placeholder
+email_address = 'xyzxxxaxbxzx123@gmail.com' 
+email_pass = 'xxx@123456'
 
 mail = imaplib.IMAP4_SSL('imap.gmail.com')
 mail.login(email_address, email_pass)
@@ -20,8 +13,7 @@ mail.login(email_address, email_pass)
 num_of_mail = 0
 
 while True:
-    # ==================================================================================================================>>>
-    # 2nd -> if any email is not comming in bot email box then wait  infinite time....
+   
     print("Waitting for website response...")
 
     # animation = ["10%", "20%", "30%", "40%", "50%", "60%", "70%", "80%", "90%", "100%"]
@@ -41,8 +33,7 @@ while True:
     id_list = mail_ids.split()
 
     if len(id_list) > num_of_mail:
-        # ================================================================================>>
-        # 1st -> if any new email came in bot email box then read that email and cature all information inside email...
+      
         print('New Mail Found...\n')
 
         for i in range(int(id_list[-1]), int(id_list[0]) -1, -1):
